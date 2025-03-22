5ac0b3e766aaf34303a54538b41a261bd0cc8d6c
Jacob Tizel <jacob.tizel@gmail.com>
2025-03-22 01:37:50 -0700

    Added yCbCr processing

---
+++ pipeline/Colourspaces.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+
+def yCbCrProcessing(image, CbMinusCrThresh = 80):
+    """
+    Uses methodologies from "Fire Recognition Using Rgb And YCbCr Color Space"
+    to detect potential fire pixels in YCbCr colour space. 
+    Returns a mask with the same size as the image.
+    """
+    YCrCb = cv2.cvtColor(image, cv2.COLOR_BGR2YCR_CB)
+
+    Y, Cr, Cb = cv2.split(YCrCb)
+
+    YGreaterThanCb = Y >= Cb
+    CrGreaterThanCb = Cr >= Cb
+
+    # meanCalcs = np.logical_and(np.logical_and(Y >= np.mean(Y), Cb <= np.mean(Cb)), Cr >= np.mean(Cr))
+    meanCalcs = np.logical_and.reduce([Y >= np.mean(Y), Cb <= np.mean(Cb), Cr >= np.mean(Cr)])
+
+    # CbMinusCrThresh should be between 0 and 224?
+    CbMinusCr = (Cb - Cr) >= CbMinusCrThresh 
+
+    chrominanceThresh = np.logical_and(Cb <= 120, Cr >= 150)
+
+    finalMask = np.logical_and.reduce([YGreaterThanCb, CrGreaterThanCb, meanCalcs, CbMinusCr, chrominanceThresh])
+    finalMask.dtype="uint8"
+    
+    return finalMask
+
+
+    
+
+
