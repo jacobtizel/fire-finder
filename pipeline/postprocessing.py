472661ff6979fe82993ce66d00049706b336d6aa
Jacob Tizel <jacob.tizel@gmail.com>
2025-03-25 13:28:09 -0700

    mask segmentation max size method
    also colour space test ipynb

--- pipeline/postprocessing.py
+++ pipeline/postprocessing.py
@@ -18,3 +18,15 @@ def opening(mask):
     strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
     opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, strel)
     return opening
+
+def largestCountourArea(mask):
+    """
+    Takes a binary mask and returns the area of the largest
+    region inside it.
+    """
+    retval, labels, stats, centroids= cv2.connectedComponentsWithStats(mask)
+    areas = stats[:, cv2.CC_STAT_AREA]
+    
+    print(max(areas))
+    
+    return max(areas)
