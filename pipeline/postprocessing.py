eca3e74f1b477466488fea324e75ed76a0eb5bda
Jacob Tizel <jacob.tizel@gmail.com>
2025-03-25 01:09:59 -0700

    Adds closing and opening methods

---
+++ pipeline/postprocessing.py
@@ -0,0 +1,20 @@
+import cv2
+
+def closing(mask):
+    """
+    Applies morphological closing to a binary mask, 
+    using a 5x5 elliptical structuring element.
+    """
+    strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+    closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, strel)
+    return closing
+
+
+def opening(mask):
+    """
+    Applies morphological opening to a binary mask, 
+    using a 5x5 elliptical structuring element.
+    """
+    strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+    opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, strel)
+    return opening
