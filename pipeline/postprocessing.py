9cc27464e1d410b30905db85093863024ebc557a
LadanTazik <ladan.tazik@gmail.com>
2025-03-31 21:56:42 -0700

    Update postprocessing.py
    made some minor changes

--- pipeline/postprocessing.py
+++ pipeline/postprocessing.py
@@ -1,21 +1,21 @@
 import cv2
 
-def closing(mask):
+def closing(mask, ksize=5):
     """
     Applies morphological closing to a binary mask, 
     using a 5x5 elliptical structuring element.
     """
-    strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+    strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (ksize, ksize))
     closing = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, strel)
     return closing
 
 
-def opening(mask):
+def opening(mask, ksize=5):
     """
     Applies morphological opening to a binary mask, 
     using a 5x5 elliptical structuring element.
     """
-    strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+    strel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (ksize, ksize))
     opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, strel)
     return opening
 
@@ -24,9 +24,10 @@ def largestCountourArea(mask):
     Takes a binary mask and returns the area of the largest
     region inside it.
     """
-    retval, labels, stats, centroids= cv2.connectedComponentsWithStats(mask)
-    areas = stats[:, cv2.CC_STAT_AREA]
-    
-    print(max(areas))
-    
+    mask = mask.astype(np.uint8)
+    retval, labels, stats, _ = cv2.connectedComponentsWithStats(mask)
+    areas = stats[1:, cv2.CC_STAT_AREA]  # Skip background (label 0)
+
+    if areas.size == 0:
+        return 0
     return max(areas)
